f6be9e519ee08d4b6d905735ef33be950b7cf293
Dani <daniyal.lilani@ontariotechu.net>
2024-10-15 14:06:29 -0400

    setting up

---
+++ lib/LoginHandler.dart
@@ -0,0 +1,6 @@
+/* This file will manage logins thru google, I will set this up later 
+https://nikhilsomansahu.medium.com/how-to-implement-google-sign-in-feature-in-flutter-app-71d957cac39b
+Idea. On first login, we have the user sign in via google. We find a way to persist their login details unless \
+they decide to log out. It seems we need some firebase dependencies, will come back to this
+
+*/
---
+++ lib/User.dart
@@ -0,0 +1,9 @@
+/*
+Username
+UserID (unique, hidden)
+Budget (class object)
+Pins (class object)
+Calender (class object), contains a list of activities
+This is just a start, we will add more as we go
+
+ */
